125ebce7be620af8a7e41fefdc3f000b2ed2038f
Luis Gerhorst <luis@luisgerhorst.de>
2013-01-23 20:01:12 +0100

    rewrote Format() and removed onclick= from HTML

--- javascript.js
+++ javascript.js
@@ -1,249 +1,191 @@
-/* Build 7 */
-
-
-// Ready Document
-
 $(document).ready(function () {
 
-    defaultView();
-    if (readCookie('settingsRemovedHTML') == '') defaultFormats();
-    else readStatus();
-    
-    checkForUpdate();
 
-    setAllTimes();
-    setInterval(setAllTimes, 1000);
-
-}); // document.ready
+// Obj Format
 
 
-// defining Object
-
-function Format() {}
-
-Format.prototype.add = function() {
-    
-    hide('#settings ul.removed .' + this.id);
+function Format(id, string, unit, time) {
 	
-	$('#settings ul.added').append('<li class="' + this.id + ' button" onclick="' + this.id + 'Format.remove();">- ' + this.string + '</li>');
+	this.setTime = function() {
+	    
+	    if ($('#clock ul li.' + id).length != 0) $('#clock ul li.' + id + ' span.number').text(time);
+		
+	}
 	
-	$('#clock ul').append('<li class="' + this.id + '"><span class="number"></span><span class="unit"> ' + this.unit + '</span></li>');
+	this.add = function() {
+	    
+	    hide('#settings ul.removed li.' + id); // hide .removed item
+		
+		$('#settings ul.added').append('<li class="' + id + ' button">- ' + string + '</li>'); // add .added item
+		
+		$('#clock ul').append('<li class="' + id + '"><span class="number"></span><span class="unit"> ' + unit + '</span></li>'); // add format
+		
+		console.log('.add called; .id=' + id + ';'); // log
+		
+	}
 	
-	console.log('.add called; .id=' + this.id + ';');
-	
-} // .add
+	var remove = function () {
+	    
+	    show('#settings ul.removed li.' + id); // show .removed item
+		
+		$('#settings ul.added li.' + id).remove(); // remove .added item
+		
+		$('#clock ul li.' + id).remove(); // remove format
+		
+		console.log('.remove called; .id=' + id + ';'); // log
+		
+	}
 
-Format.prototype.remove = function() {
-    
-    show('#settings .removed li.' + this.id);
-	
-	$('#settings .added li.' + this.id).remove();
-	
-	$('#clock ul li.' + this.id).remove();
-	
-	console.log('.remove called; .id=' + this.id + ';');
-	
-} // .remove
 
-
-Format.prototype.setTime = function() {
-    
-    if ($('#clock ul li.' + this.id).length != 0) $('#clock ul li.' + this.id + ' span.number').text(this.setTimeFunction);
+	$('#settings ul.removed').append('<li class="' + id + ' button">+ ' + string + '</li>'); // add .removed item
 	
-} // .setTime
-
-
-// creating Objects
-
-var secondFormat = new Format();
-secondFormat.id = 'second';
-secondFormat.string = 'Second';
-secondFormat.unit = 's';
-secondFormat.setTimeFunction = function() {
 	
-	return getMomentFormat('ss');
-	
+	$('#settings ul.removed li.' + id).click(this.add); // on click on .removed item
+	$('#settings ul.added li.' + id).click(remove); // on click on .added item
+
 }
 
-var secondDayFormat = new Format();
-secondDayFormat.id = 'secondDay';
-secondDayFormat.string = 'Second of Day';
-secondDayFormat.unit = 's/d';
-secondDayFormat.setTimeFunction = function() {
-    
-    var hours='', minutes='', seconds='', thousands='', hundreds='';
-    
-    hours = parseInt(getMomentFormat('HH')) * 60 * 60;
-    minutes = parseInt(getMomentFormat('mm')) * 60;
-    seconds = hours + minutes + parseInt(getMomentFormat('ss'));
-    
-    thousands = Math.floor(seconds/1000);
-    hundreds = seconds - thousands * 1000;
-    
-    if (thousands == 0) seconds = hundreds;
-    else seconds = thousands + " " + hundreds;
-    
-    return seconds;
-    
-}
 
-var secondYearFormat = new Format();
-secondYearFormat.id = 'secondYear';
-secondYearFormat.string = 'Second of Year';
-secondYearFormat.unit = 's/y';
-secondYearFormat.setTimeFunction = function() {
-    
-    var days='', hours='', minutes='', seconds='', thousands='', hundreds='';
-    
-    days = parseInt(getMomentFormat('DDDD')) * 24 * 60 * 60;
-    hours = parseInt(getMomentFormat('HH')) * 60 * 60;
-    minutes = parseInt(getMomentFormat('mm')) * 60;
-    seconds = days + hours + minutes + parseInt(getMomentFormat('ss'));
-    
-    millions = Math.floor(seconds/1000000);
-    thousands = Math.floor(seconds/1000) - millions * 1000;
-    hundreds = seconds - millions * 1000000 - thousands * 1000;
-    
-    if (millions != 0 && thousands != 0) seconds = millions + " " + thousands + " " + hundreds;
-    if (millions == 0 && thousands != 0) seconds = thousands + " " + hundreds;
-    if (millions == 0 && thousands == 0) seconds = hundreds;
-    
-    return seconds;
-    
-}
+// create Formats
 
-var minuteFormat = new Format();
-minuteFormat.id = 'minute';
-minuteFormat.string = 'Minute';
-minuteFormat.unit = 'm';
-minuteFormat.setTimeFunction = function() {
+var formats = {
 	
-	return getMomentFormat('mm');
+	second: new Format('second', 'Second', 's', function() {
+		
+		return moment().format('ss');
 	
-}
+	}),
+	
+	secondDay: new Format('secondDay', 'Second of Day', 's/d', function() {
+	    
+	    var hours='', minutes='', seconds='', thousands='', hundreds='';
+	    
+	    hours = parseInt(moment().format('HH')) * 60 * 60;
+	    minutes = parseInt(moment().format('mm')) * 60;
+	    seconds = hours + minutes + parseInt(moment().format('ss'));
+	    
+	    thousands = Math.floor(seconds/1000);
+	    hundreds = seconds - thousands * 1000;
+	    
+	    if (thousands == 0) seconds = hundreds;
+	    else seconds = thousands + " " + hundreds;
+	    
+	    return seconds;
+	    
+	}),
+	
+	secondYear: new Format('secondYear', 'Second of Year', 's/y', function() {
+	    
+	    var days='', hours='', minutes='', seconds='', thousands='', hundreds='';
+	    
+	    days = parseInt(moment().format('DDDD')) * 24 * 60 * 60;
+	    hours = parseInt(moment().format('HH')) * 60 * 60;
+	    minutes = parseInt(moment().format('mm')) * 60;
+	    seconds = days + hours + minutes + parseInt(moment().format('ss'));
+	    
+	    millions = Math.floor(seconds/1000000);
+	    thousands = Math.floor(seconds/1000) - millions * 1000;
+	    hundreds = seconds - millions * 1000000 - thousands * 1000;
+	    
+	    if (millions != 0 && thousands != 0) seconds = millions + " " + thousands + " " + hundreds;
+	    if (millions == 0 && thousands != 0) seconds = thousands + " " + hundreds;
+	    if (millions == 0 && thousands == 0) seconds = hundreds;
+	    
+	    return seconds;
+	    
+	}),
+	
+	minute: new Format('minute', 'Minute', 'm', function() {
+		
+		return moment().format('mm');
+		
+	}),
+	
+	minuteDay: new Format('minuteDay', 'Minute of Day', 'm/d', function() {
+		
+		var hours='', minutes='', thousands='', hundreds='';
+	    
+	    hours = parseInt(moment().format('HH')) * 60;
+	    minutes = hours + parseInt(moment().format('mm'));
+	    
+	    thousands = Math.floor(minutes/1000);
+	    hundreds = minutes - thousands * 1000;
+	    
+	    if (thousands == 0) minutes = hundreds;
+	    else minutes = thousands + " " + hundreds;
+	    
+	    return minutes;
+		
+	}),
+	
+	hour: new Format('hour', 'Hour', 'h', function() {
+		
+		return moment().format('HH');
+		
+	}),
+	
+	dayWeek: new Format('dayWeek', 'Day of Week', 'd/w', function() {
+		
+		return moment().format('d');
+		
+	}),
+	
+	day: new Format('day', 'Day', 'd', function() {
+		
+		return moment().format('DD');
+		
+	}),
+	
+	dayYear: new Format('dayYear', 'Day of Year', 'd/y', function() {
+		
+		return moment().format('DDDD');
+		
+	}),
+	
+	week: new Format('week', 'Week', 'w', function() {
+		
+		return moment().format('ww');
+		
+	}),
+	
+	month: new Format('month', 'Month', 'M', function() {
+		
+		return moment().format('MM');
+		
+	}),
+	
+	year: new Format('year', 'Year', 'y', function() {
+		
+		return moment().format('YYYY');
+		
+	})
+	
+};
 
-var minuteDayFormat = new Format();
-minuteDayFormat.id = 'minuteDay';
-minuteDayFormat.string = 'Minute of Day';
-minuteDayFormat.unit = 'm/d';
-minuteDayFormat.setTimeFunction = function() {
-	
-	var hours='', minutes='', thousands='', hundreds='';
-    
-    hours = parseInt(getMomentFormat('HH')) * 60;
-    minutes = hours + parseInt(getMomentFormat('mm'));
-    
-    thousands = Math.floor(minutes/1000);
-    hundreds = minutes - thousands * 1000;
-    
-    if (thousands == 0) minutes = hundreds;
-    else minutes = thousands + " " + hundreds;
-    
-    return minutes;
-	
-}
-
-var hourFormat = new Format();
-hourFormat.id = 'hour';
-hourFormat.string = 'Hour';
-hourFormat.unit = 'h';
-hourFormat.setTimeFunction = function() {
-	
-	return getMomentFormat('HH');
-	
-}
-
-var dayWeekFormat = new Format();
-dayWeekFormat.id = 'dayWeek';
-dayWeekFormat.string = 'Day of Week';
-dayWeekFormat.unit = 'd/w';
-dayWeekFormat.setTimeFunction = function() {
-	
-	return getMomentFormat('d');
-	
-}
-
-var dayFormat = new Format();
-dayFormat.id = 'day';
-dayFormat.string = 'Day';
-dayFormat.unit = 'd';
-dayFormat.setTimeFunction = function() {
-	
-	return getMomentFormat('DD');
-	
-}
-
-var dayYearFormat = new Format();
-dayYearFormat.id = 'dayYear';
-dayYearFormat.string = 'Day of Year';
-dayYearFormat.unit = 'd/y';
-dayYearFormat.setTimeFunction = function() {
-	
-	return getMomentFormat('DDDD');
-	
-}
-
-var weekFormat = new Format();
-weekFormat.id = 'week';
-weekFormat.string = 'Week';
-weekFormat.unit = 'w';
-weekFormat.setTimeFunction = function() {
-	
-	return getMomentFormat('ww');
-	
-}
-
-var monthFormat = new Format();
-monthFormat.id = 'month';
-monthFormat.string = 'Month';
-monthFormat.unit = 'M';
-monthFormat.setTimeFunction = function() {
-	
-	return getMomentFormat('MM');
-	
-}
-
-var yearFormat = new Format();
-yearFormat.id = 'year';
-yearFormat.string = 'Year';
-yearFormat.unit = 'y';
-yearFormat.setTimeFunction = function() {
-	
-	return getMomentFormat('YYYY');
-	
-}
-
-function getMomentFormat(format) {
-	return moment().format(format);
-}
 
 // all
 
 function setAllTimes() {
-	secondFormat.setTime();
-	secondDayFormat.setTime();
-	secondYearFormat.setTime();
-	minuteFormat.setTime();
-	minuteDayFormat.setTime();
-	hourFormat.setTime();
-	dayWeekFormat.setTime();
-	dayFormat.setTime();
-	dayYearFormat.setTime();
-	weekFormat.setTime();
-	monthFormat.setTime();
-	yearFormat.setTime();
+	
+	for (var id in formats) {
+		formats[id].setTime();
+	}
+	
 }
 
 // buttons
 
-function settingsIconClick() {
+$('#settings-icon').click(showSettings);
+$('#done-icon').click(showClockAndSave);
+
+function showSettings() {
 	hide('#clock');
     show('#done-icon');
     show('#settings');
     hide('#settings-icon');
 }
 	
-function doneIconClick() {
+function showClockAndSave() {
     show('#clock');
     hide('#done-icon');
     hide('#settings');
@@ -263,20 +205,6 @@ function readStatus() {
 	$('#clock ul').html(readCookie('clockHTML'));
 	$('#settings ul.added').html(readCookie('settingsAddedHTML'));
 	$('#settings ul.removed').html(readCookie('settingsRemovedHTML'));
-}
-
-// default
-
-function defaultView() {
-    show('#clock');
-    hide('#done-icon');
-    hide('#settings');
-    show('#settings-icon');
-}
-
-function defaultFormats() {
-    secondDayFormat.add();
-    weekFormat.add();
 }
 
 // set / read Cookie
@@ -327,3 +255,32 @@ function updateApplication(event) {
     window.applicationCache.swapCache();
     window.location.reload();
 }
+
+// defaults
+
+function showClock() {
+    show('#clock');
+    hide('#done-icon');
+    hide('#settings');
+    show('#settings-icon');
+}
+
+function addDefaultFormats() {
+    formats.secondDay.add();
+    formats.week.add();
+}
+
+
+// Start App
+
+showClock();
+if (readCookie('settingsRemovedHTML') == '') addDefaultFormats();
+else readStatus();
+    
+checkForUpdate();
+
+setAllTimes();
+setInterval(setAllTimes, 1000);
+
+
+}); // document.ready
