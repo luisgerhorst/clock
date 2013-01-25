13ca1e54f87bfe8c7227ca53249860ce34d1b91b
Luis Gerhorst <luis@luisgerhorst.de>
2013-01-25 19:48:26 +0100

    new update mechanism and more objects

--- javascript.js
+++ javascript.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {
 
 
-// Obj Format
-
+// Objects
 
 function Format(id, string, unit, time) {
 	
@@ -14,7 +13,7 @@ function Format(id, string, unit, time) {
 	
 	this.add = function() {
 	    
-	    hide('#settings ul.removed li.' + id); // hide .removed item
+	    $('#settings ul.removed li.' + id).hide(); // hide .removed item
 		
 		$('#settings ul.added').append('<li class="' + id + ' button" data-id="' + id + '">- ' + string + '</li>'); // add .added item
 		$('#settings ul.added li.' + id).click(remove); // add .added item listener
@@ -27,10 +26,8 @@ function Format(id, string, unit, time) {
 	
 	var remove = function () {
 	    
-	    show('#settings ul.removed li.' + id); // show .removed item
-		
+	    $('#settings ul.removed li.' + id).show(); // show .removed item
 		$('#settings ul.added li.' + id).remove(); // remove .added item
-		
 		$('#clock ul li.' + id).remove(); // remove format
 		
 		console.log('remove called, id=' + id + ''); // log
@@ -40,12 +37,158 @@ function Format(id, string, unit, time) {
 	$('#settings ul.removed').append('<li class="' + id + ' button">+ ' + string + '</li>'); // add .removed item
 	$('#settings ul.removed li.' + id).click(this.add); // add .removed item listener
 	
-	console.log('new Format created, id=' + id);
+	// console.log('new Format created, id=' + id);
 
 }
 
 
-// create Formats
+function AllFormats() {
+	
+	this.setTime = function () {
+	
+		for (var id in formats) {
+			formats[id].setTime();
+		}
+		
+	}
+	
+}
+
+
+function Cookie() {
+	
+	this.save = function (key, value) {
+	
+		document.cookie = 'clock_' + key + '=' + encodeURIComponent(value) + '; expires=' + new Date(moment() + 1000*60*60*24*360).toGMTString() + ';';
+		
+	}
+	
+	this.read = function (key) {
+	
+	    key =  'clock_' + key;
+	    
+	 	var value = '';
+		if(document.cookie) {
+	       	var array = document.cookie.split((escape(key) + '=')); 
+	       	if(2 <= array.length) {
+	           	var array2 = array[1].split(';');
+	       		value  = unescape(array2[0]);
+	       	}
+		}
+		
+		return decodeURIComponent(value);
+		
+	}
+	
+}
+
+
+function Settings() {
+
+	var show = function () {
+		$('#clock').hide();
+	    $('#done-icon').show();
+	    $('#settings').show();
+	    $('#settings-icon').hide();
+	}
+	
+	$('#settings-icon').click(show);
+	
+	this.hide = function () {
+	    $('#clock').show();
+	    $('#done-icon').hide();
+	    $('#settings').hide();
+	    $('#settings-icon').show();
+	}
+	
+	var save = function () {
+		
+		var added = {};
+		
+		$('#settings ul.added li').each( function () {
+			added[Object.keys(added).length] = $(this).attr('data-id');
+		});
+		
+		added = JSON.stringify(added);
+		cookie.save('added', added);
+		
+		console.log("saved Status, " + added);
+		
+	}
+	
+	var Settings = this;
+	
+	$('#done-icon').click(function () {
+		Settings.hide();
+		save();
+		console.log('#done-icon clicked');
+	});
+	
+	var read = function () {
+	
+		var added = cookie.read('added');
+		added = JSON.parse(added);
+		
+		var id;
+		for (var count in added) {
+			id = added[count];
+			formats[id].add();
+		}
+		
+	}
+	
+	this.check = function () {
+	
+		if (cookie.read('added') == '') {
+			formats.secondDay.add();
+			formats.week.add();
+		}
+		
+		else read();
+	
+	}
+	
+}
+
+
+function Updater() {
+
+	$('#update-available').hide();
+
+	this.install = function (event) {
+	    
+	    if (window.applicationCache.status != 4) return; // zur sicherheit
+	    window.applicationCache.removeEventListener('updateready', updater.install);
+	    window.applicationCache.swapCache();
+	    
+	    console.log('Update found, will be installed after next refresh.');
+	    
+	    $('#update-available').show();
+	    $('#update-available').click(function () { window.location.reload(); });
+	    
+	}
+
+	this.check = function () {
+		
+		console.log('Checking for update ...');
+		
+	    if (window.applicationCache != undefined && window.applicationCache != null) {
+	        window.applicationCache.addEventListener('updateready', updater.install);
+	    }
+	    
+	}
+
+}
+
+
+// Utilities
+
+function numberWithSeperator(string, seperator) {
+    return string.toString().replace(/\B(?=(\d{3})+(?!\d))/g, seperator);
+}
+
+
+// Start App
 
 var formats = {
 	
@@ -57,17 +200,13 @@ var formats = {
 	
 	secondDay: new Format('secondDay', 'Second of Day', 's/d', function() {
 	    
-	    var hours='', minutes='', seconds='', thousands='', hundreds='';
+	    var hours='', minutes='', seconds='';
 	    
 	    hours = parseInt(moment().format('HH')) * 60 * 60;
 	    minutes = parseInt(moment().format('mm')) * 60;
 	    seconds = hours + minutes + parseInt(moment().format('ss'));
 	    
-	    thousands = Math.floor(seconds/1000);
-	    hundreds = seconds - thousands * 1000;
-	    
-	    if (thousands == 0) seconds = hundreds;
-	    else seconds = thousands + " " + hundreds;
+	    seconds = numberWithSeperator(seconds, ' ');
 	    
 	    return seconds;
 	    
@@ -75,7 +214,7 @@ var formats = {
 	
 	secondYear: new Format('secondYear', 'Second of Year', 's/y', function() {
 	    
-	    var days='', hours='', minutes='', seconds='', thousands='', hundreds='';
+	    var days='', hours='', minutes='', seconds='';
 	    
 	    days = parseInt(moment().format('DDDD'))-1; // -1 because the first day of the year is 1, not 0
 	    days = days * 24 * 60 * 60;
@@ -83,13 +222,7 @@ var formats = {
 	    minutes = parseInt(moment().format('mm')) * 60;
 	    seconds = days + hours + minutes + parseInt(moment().format('ss'));
 	    
-	    millions = Math.floor(seconds/1000000);
-	    thousands = Math.floor(seconds/1000) - millions * 1000;
-	    hundreds = seconds - millions * 1000000 - thousands * 1000;
-	    
-	    if (millions != 0 && thousands != 0) seconds = millions + " " + thousands + " " + hundreds;
-	    if (millions == 0 && thousands != 0) seconds = thousands + " " + hundreds;
-	    if (millions == 0 && thousands == 0) seconds = hundreds;
+	    seconds = numberWithSeperator(seconds, ' ');
 	    
 	    return seconds;
 	    
@@ -103,16 +236,12 @@ var formats = {
 	
 	minuteDay: new Format('minuteDay', 'Minute of Day', 'm/d', function() {
 		
-		var hours='', minutes='', thousands='', hundreds='';
+		var hours='', minutes='';
 	    
 	    hours = parseInt(moment().format('HH')) * 60;
 	    minutes = hours + parseInt(moment().format('mm'));
 	    
-	    thousands = Math.floor(minutes/1000);
-	    hundreds = minutes - thousands * 1000;
-	    
-	    if (thousands == 0) minutes = hundreds;
-	    else minutes = thousands + " " + hundreds;
+	    minutes = numberWithSeperator(minutes, ' ');
 	    
 	    return minutes;
 		
@@ -144,7 +273,7 @@ var formats = {
 	
 	week: new Format('week', 'Week', 'w', function() {
 		
-		return moment().format('ww');
+		return parseInt(moment().format('ww'));
 		
 	}),
 	
@@ -162,141 +291,17 @@ var formats = {
 	
 };
 
-
-// all
-
-function setAllTimes() {
-	
-	for (var id in formats) {
-		formats[id].setTime();
-	}
-	
-}
-
-// buttons
-
-$('#settings-icon').click(showSettings);
-$('#done-icon').click(showClockAndSave);
-
-function showSettings() {
-	hide('#clock');
-    show('#done-icon');
-    show('#settings');
-    hide('#settings-icon');
-}
-	
-function showClockAndSave() {
-    show('#clock');
-    hide('#done-icon');
-    hide('#settings');
-    show('#settings-icon');
-    saveStatus();
-}
-
-// save & read Status
-
-function saveStatus() {
-	
-	var added = {};
-	
-	$('#settings ul.added li').each( function () {
-		added[Object.keys(added).length] = $(this).attr('data-id');
-	});
-	
-	added = JSON.stringify(added);
-	setCookie('added', added);
-	
-	console.log("saved Status, " + added);
-	
-}
-
-function readStatus() {
-
-	var added = readCookie('added');
-	added = JSON.parse(added);
-	
-	var id;
-	for (var count in added) {
-		id = added[count];
-		formats[id].add();
-	}
-	
-}
-
-// set / read Cookie
-
-function setCookie(key, value) {
-	document.cookie = 'clock_' + key + '=' + encodeURIComponent(value) + '; expires=' + new Date(moment() + 1000*60*60*24*360).toGMTString() + ';';
-}
-
-function readCookie(key) {
-    key =  'clock_' + key;
- 	var value = '';
-	if(document.cookie) {
-       	var array = document.cookie.split((escape(key) + '=')); 
-       	if(2 <= array.length) {
-           	var array2 = array[1].split(';');
-       		value  = unescape(array2[0]);
-       	}
-	}
-	return decodeURIComponent(value);
-}
-
-// show / hide
-
-function show(object) {
-	$(object).removeClass('hide');
-}
-
-function hide(object) {
-	$(object).addClass('hide');
-}
-
-// update Check
-
-function checkForUpdate() {
-    if (window.applicationCache != undefined && window.applicationCache != null) {
-        window.applicationCache.addEventListener('updateready', updateApplication);
-    }
-}
-
-function updateApplication(event) {
-    if (window.applicationCache.status != 4) return; // zur sicherheit
-    var response = confirm("An update for this web app is available, do you want to install it now or skip this version? Your data (added and removed time formats) will be lost.");
-    if (response != true) return;
-    document.cookie = 'clock_clockHTML=; expires=Thu, 01-Jan-70 00:00:01 GMT;';
-    document.cookie = 'clock_settingsRemovedHTML=; expires=Thu, 01-Jan-70 00:00:01 GMT;';
-    document.cookie = 'clock_settingsAddedHTML=; expires=Thu, 01-Jan-70 00:00:01 GMT;';
-    window.applicationCache.removeEventListener('updateready', updateApplication);
-    window.applicationCache.swapCache();
-    window.location.reload();
-}
-
-// defaults
-
-function showClock() {
-    show('#clock');
-    hide('#done-icon');
-    hide('#settings');
-    show('#settings-icon');
-}
-
-function addDefaultFormats() {
-    formats.secondDay.add();
-    formats.week.add();
-}
-
-
-// Start App
-
-showClock();
-if (readCookie('added') == '') addDefaultFormats();
-else readStatus();
+var allFormats = new AllFormats();
+var cookie = new Cookie();
+var settings = new Settings();
+var updater = new Updater();
     
-checkForUpdate();
+updater.check();
+settings.check();
+settings.hide();
 
-setAllTimes();
-setInterval(setAllTimes, 1000);
+allFormats.setTime();
+setInterval(allFormats.setTime, 1000);
 
 
-}); // document.ready
+});
